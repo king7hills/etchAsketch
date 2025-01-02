99e40c82750cff961c19f69e521069e5dfd1e07b
king7hills <haasjh1@gmail.com>
2025-01-02 11:40:25 -0500

    sorted grid removal and reset. added prompt to change grid size

--- etchAsketch.js
+++ etchAsketch.js
@@ -1,28 +1,59 @@
-const divContainer = document.querySelector('#gridContainer');
+let divContainer = document.querySelector('#gridContainer');
 
-//makeColumnDivs(3);
-
-
-
-
-function makeRowDivs (number) {
+//Grid function. Row creation with nested column creation. Default 16.
+function makeGrid (number = 16) {
     for (let i = 0; i < number; i++) {
         const rowDiv = document.createElement('div');
         divContainer.appendChild(rowDiv);
-        rowDiv.classList.add('rowDiv');
+        rowDiv.classList.add('rowDiv', 'grid');
         
         function makeColumnDivs (number) {
             for (let i = 0; i < number;  i++) {
                 const newDiv = document.createElement('div');
                 rowDiv.appendChild(newDiv);
-                newDiv.classList.add('gridBlock', 'columnDiv');
+                newDiv.classList.add('gridBlock', 'columnDiv', 'grid');
             }
         }
 
         makeColumnDivs(number);
     }
 }
+//default grid creation
+makeGrid();
 
 
+const docBody = divContainer.parentNode;
 
-makeRowDivs(16);
+//previous grid removal
+function removeGrid() {
+    docBody.removeChild(divContainer);
+}
+//restoration of container div
+function restoreGrid() {
+    const renewedDiv = document.createElement('div');
+    docBody.appendChild(renewedDiv);
+    renewedDiv.id = 'gridContainer';
+    divContainer = renewedDiv;
+}
+//wrapper function for grid reset
+function gridReset() {
+    removeGrid();
+    restoreGrid();
+}
+
+const gridButton = document.querySelector('#changeGrid');
+let gridSize = ''
+
+function changeSize () {
+    let gridInput = prompt('Input grid size between 1-100:');
+    gridSize = Number(gridInput);
+    if (gridSize <= 100) {
+    } else {
+        alert("Invalid number");
+        return;
+    };
+    gridReset();
+    makeGrid(gridSize);
+}
+
+gridButton.addEventListener('click', () => changeSize());
